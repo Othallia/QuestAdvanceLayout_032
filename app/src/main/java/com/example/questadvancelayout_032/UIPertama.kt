580bbb81a4e005f8bcfc0cc4140132cf414e10df
Othallia <vianiputriocta6@gmail.com>
2025-10-24 15:34:23 +0700

    menambahkan resource

--- app/src/main/java/com/example/questadvancelayout_032/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_032/UIPertama.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -16,6 +17,8 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+
+private fun Modifier.fillMaxWidht(fraction: Float) {}
 
 @Composable
 fun  ActivitasPertama(modifier: Modifier) {
